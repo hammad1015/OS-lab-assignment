e32f04ecaf962761176799d603a97fb1c1daf316
pixus99 <45166466+pixus99@users.noreply.github.com>
2020-11-26 20:56:45 +0500

    created the struct for content and metadata

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 import os
 import json
 
-root = json.load(open('sample.data'))
+
+def save(data):
+    metaData = json.dumps(data)
+    FILE.seek(0)
+    FILE.write(metaData[:-1])
+    FILE.write((maxMetaDataSize - len(metaData)) * ' ')
+    FILE.write('}')
+    FILE.seek(0)
+
+
+maxMetaDataSize    = 2**10
+
+if not os.path.exists('sample.data'):
+    FILE = open('sample.data', 'w+')
+    save({})
+else:
+    FILE = open('sample.data', 'r+')
+
+
+root = FILE.read(maxMetaDataSize)
+root = json.loads(root)
+
 CWD  = root
 
-
 def Create(fname):
-    CWD[fname] = ''
+    CWD[fname] = [[]]
     
 
 def Delete(fname):
@@ -27,6 +47,8 @@ def Open(fname, mode= 'w'):
 
 def Close():
     pass
+
+
 
 
 
