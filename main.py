d07648a53b666fd10927e4212ea2624d53349138
UsamaEjaz0 <uejaz0@gmail.com>
2020-11-26 19:42:33 +0500

    Added CLI

--- main.py
+++ main.py
@@ -46,3 +46,38 @@ def ():
 def ():
     pass
 '''
+
+
+
+# CLI 
+
+while True:
+    statement = input(">").split(" ")
+    try:
+        command = statement[0].lower()
+
+        if command == "create":
+            fname = statement[1]
+            Create(fname)
+        elif command == "delete":
+            fname = statement[1]
+            Delete(fname)
+        elif command == "mkdir":
+            dir_name = statement[1]
+            Mkdir(dir_name)
+        elif command == "chdir":
+            dir_name = statement[1]
+            Chdir(dir_name)
+        elif command == "move":
+            source_fname = statement[1]
+            target_fname = statement[2]
+            Move(source_fname, target_fname)
+        elif command == "open":
+            fname = statement[1]
+            mode = statement[2]
+            Open(fname, mode)
+        elif command == "close":
+            fname = statement[1]   
+            Close(fname) 
+    except:
+        print("Invalid syntax!")
